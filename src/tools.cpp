990199e7d778c98ed6a025b889072bd5c9bdfff5
citlaligm <gmtlali88@gmail.com>
2017-03-06 00:37:21 -0800

    change implementation of Jacobian to avoid repeat operations. Put Hj to 0's if measurement is zero in x or y

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,7 +15,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//  * the estimation vector size should equal ground truth vector size	
 	try
     {
-        if (estimations.size()==0 |estimations.size()!=ground_truth.size() ) throw 20;
+        if ((estimations.size()==0) |(estimations.size()!=ground_truth.size()) ) throw 20;
     }
         catch (int e)
     {
@@ -53,28 +53,27 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	float py = x_state(1);
 	float vx = x_state(2);
 	float vy = x_state(3);
+
+	float c1 = px*px+py*py;
+	float c2 = sqrt(c1);
+	float c3 = (c1*c2);
     
 	//check division by zero
 	try
     {
-        if(px == 0 | py == 0) throw 20;
+        if(c1<0.0001)
+        {
+        	Hj << 0, 0, 0, 0,
+        	      0, 0, 0, 0,
+        		  0, 0, 0, 0;
+        	throw 20;
+        }
         else 
         {
-            float px2 = px*px;
-            float py2 = py*py;
-            float p_00 = px/sqrt(px2 +py2);
-            float p_01 = py/sqrt(px2 +py2);
-            float p_10 = -py/(px2 +py2);
-            float p_11 = px/(px2 +py2);
-            float p_20 = (py*(vx*py-vy*px))/pow((px2 +py2),1.5);
-            float p_21 = (px*(vy*px-vx*py))/pow((px2 +py2),1.5);
-            float p_22 = px/sqrt(px2 +py2);
-            float p_23 = px/sqrt(px2 +py2);
-            
-            //compute the Jacobian matrix
-            Hj << p_00, p_01, 0, 0,
-			  p_10, p_11, 0, 0,
-			  p_20, p_21, p_22, p_23;
+        	//compute the Jacobian matrix
+        	Hj << (px/c2), (py/c2), 0, 0,
+        		  -(py/c1), (px/c1), 0, 0,
+				  py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
             
         }
             
